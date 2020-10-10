dc491689931bd0c6be2e91a985089d3a22fe0609
Ayatea <ayatea1223@gmail.com>
2020-10-10 14:42:38 +0900

    :tada: [#1] 新規SpringBootプロジェクトを作成

---
+++ src/main/java/com/ayatea/jaudiotagger_tutorial/JaudiotaggerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ayatea.jaudiotagger_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaudiotaggerTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaudiotaggerTutorialApplication.class, args);
+	}
+
+}
